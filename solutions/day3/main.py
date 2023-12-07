d7875677b301454c0ac40dc8e51e4492044ce3cb
Akshay Verma <15175239+akshay-verma@users.noreply.github.com>
2023-12-07 17:06:50 +0530

    Day3 Part 2 Solution

--- solutions/day3/main.py
+++ solutions/day3/main.py
@@ -1,3 +1,4 @@
+import math
 from pathlib import Path
 
 input_file = Path(__file__).resolve().parent / "input.txt"
@@ -32,27 +33,40 @@ def part_1(data: list[str]):
         for col in range(len(matrix[0])):
             if matrix[row][col].isdigit() or matrix[row][col] == '.':
                 continue
-            #print(f"{row} {col} --> {matrix[row][col]}")
             inputs = [(row, col + 1), (row, col -1), (row -1, col), (row-1,col-1),(row-1,col+1), (row + 1, col - 1), (row + 1, col + 1)]
             for inp in inputs:
-                #print(tracked, inp[0], inp[1])
                 if matrix[inp[0]][inp[1]].isdigit() and (inp[0], inp[1]) not in tracked:
-                    #print("NOT TRACKED")
                     num = find_largest_number(matrix, inp[0], inp[1], tracked)
                     if num:
                         numbers.append(num)
-            #print("=" * 80)
     return sum(numbers)
 
 
-TEST_INPUT = ["467..114..", "...*......", "..35..633.", "......#...", "617*......", ".....+.58.", "..592.....", "......755.", "...$.*....", ".664.598.."]
+def part_2(data: list[str]):
+    matrix = create_matrix(data)
+    gear_ratios = []
+    tracked = set()
+    for row in range(len(matrix)):
+        for col in range(len(matrix[0])):
+            if matrix[row][col] != '*':
+                continue
+            adj_nums = []
+            inputs = [(row, col + 1), (row, col -1), (row -1, col), (row-1,col-1),(row-1,col+1), (row + 1, col - 1), (row + 1, col + 1)]
+            for inp in inputs:
+                if matrix[inp[0]][inp[1]].isdigit() and (inp[0], inp[1]) not in tracked:
+                    num = find_largest_number(matrix, inp[0], inp[1], tracked)
+                    if num:
+                        adj_nums.append(num)
+            if len(adj_nums) > 1:
+                gear_ratios.append(math.prod(adj_nums))
+    return sum(gear_ratios)
 
 
 if __name__ == "__main__":
     data = input_file.read_text().splitlines()
     result = part_1(data)
-    print(result)
+    print(f"Part1: {result}")
 
-    #result = part_1(TEST_INPUT)
-    #print(result)
+    result = part_2(data)
+    print(f"Part2: {result}")
 
