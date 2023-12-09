253abd59976d7ba4282bf4d2d790af03304136e7
Akshay Verma <15175239+akshay-verma@users.noreply.github.com>
2023-12-09 20:42:24 +0530

    Day4 Part2 Solution

--- solutions/day4/main.py
+++ solutions/day4/main.py
@@ -28,12 +28,27 @@ def part_1(data):
     return result
 
 
-TEST_INPUT = ["Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53", "Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19", "Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1", "Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83", "Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36", "Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11"]
+def part_2(data):
+    count = defaultdict(lambda: 1)
+    for idx, line in enumerate(data):
+        x, y = line.split("|")
+        x = x.split(":")[1]
+        win = create_map(x)
+        mine = create_map(y)
+        card_num = idx + 1
+        i = 0
+        for num in mine:
+            if num in win:
+                cur_count = count[card_num]
+                new_index = card_num + i + 1
+                count[new_index] += 1 * cur_count
+                i += 1
+    return sum(count.values())
+
 
 if __name__ == "__main__":
-    #result = part_1(input_file.read_text().splitlines())
-    #print(result)
-    
-    result = part_1(TEST_INPUT)
-    print(result)
+    result = part_1(input_file.read_text().splitlines())
+    print(f"Part1: {result}")
 
+    result = part_2(input_file.read_text().splitlines())
+    print(f"Part2: {result}")
